804428533047106bf704c081e3a1558e575fb958
j-e-ff <118487175+j-e-ff@users.noreply.github.com>
2025-03-26 00:04:52 -0700

    Checkpoint 1 Files
    Code was used from Canvas

---
+++ FPCameraController.java
@@ -0,0 +1,208 @@
+import org.lwjgl.util.vector.Vector3f;
+import org.lwjgl.input.Keyboard;
+import org.lwjgl.input.Mouse;
+import org.lwjgl.opengl.Display;
+import static org.lwjgl.opengl.GL11.*;
+import org.lwjgl.Sys;
+
+public class FPCameraController {
+    private Vector3f position = null;
+    private Vector3f lPosition = null;
+    private float yaw = 0.0f;
+    private float pitch = 0.0f;
+
+    public FPCameraController(float x, float y, float z) {
+        position = new Vector3f(x, y, z);
+        lPosition = new Vector3f(x, y, z);
+        lPosition.x = 0f;
+        lPosition.y = 15f;
+        lPosition.z = 0f;
+    }
+
+    public void yaw(float amount) {
+        yaw += amount;
+    }
+
+    public void pitch(float amount) {
+        pitch -= amount;
+    }
+
+    public void walkForward(float distance) {
+        float xOffset = distance * (float) Math.sin(Math.toRadians(yaw));
+        float zOffset = distance * (float) Math.cos(Math.toRadians(yaw));
+        position.x -= xOffset;
+        position.z += zOffset;
+    }
+
+    public void walkBackwards(float distance) {
+        float xOffset = distance * (float) Math.sin(Math.toRadians(yaw));
+        float zOffset = distance * (float) Math.cos(Math.toRadians(yaw));
+        position.x += xOffset;
+        position.z -= zOffset;
+    }
+
+    public void strafeLeft(float distance) {
+        float xOffset = distance * (float) Math.sin(Math.toRadians(yaw - 90));
+        float zOffset = distance * (float) Math.cos(Math.toRadians(yaw - 90));
+        position.x -= xOffset;
+        position.z += zOffset;
+    }
+
+    public void strafeRight(float distance) {
+        float xOffset = distance * (float) Math.sin(Math.toRadians(yaw + 90));
+        float zOffset = distance * (float) Math.cos(Math.toRadians(yaw + 90));
+        position.x -= xOffset;
+        position.z += zOffset;
+    }
+
+    public void moveUp(float distance) {
+        position.y -= distance;
+    }
+
+    public void moveDown(float distance) {
+        position.y += distance;
+    }
+
+    public void lookThrough() {
+        glRotatef(pitch, 1.0f, 0.0f, 0.0f);
+        glRotatef(yaw, 0.0f, 1.0f, 0.0f);
+        glTranslatef(position.x, position.y, position.z);
+    }
+
+    public void gameLoop() {
+        FPCameraController camera = new FPCameraController(0, 0, 0);
+        float dx, dy, dt, lastTime = 0.0f;
+        long time;
+        float mouseSensitivity = 0.09f;
+        float movementSpeed = 0.35f;
+
+        Mouse.setGrabbed(true);
+
+        while (!Display.isCloseRequested() && !Keyboard.isKeyDown(Keyboard.KEY_ESCAPE)&& !Keyboard.isKeyDown(Keyboard.KEY_Q)) {
+            time = Sys.getTime();
+            lastTime = time;
+
+            dx = Mouse.getDX();
+            dy = Mouse.getDY();
+            camera.yaw(dx * mouseSensitivity);
+            camera.pitch(dy * mouseSensitivity);
+
+            if (Keyboard.isKeyDown(Keyboard.KEY_W)) {
+                camera.walkForward(movementSpeed);
+            }
+            if (Keyboard.isKeyDown(Keyboard.KEY_S)) {
+                camera.walkBackwards(movementSpeed);
+            }
+            if (Keyboard.isKeyDown(Keyboard.KEY_A)) {
+                camera.strafeLeft(movementSpeed);
+            }
+            if (Keyboard.isKeyDown(Keyboard.KEY_D)) {
+                camera.strafeRight(movementSpeed);
+            }
+            if (Keyboard.isKeyDown(Keyboard.KEY_SPACE)) {
+                camera.moveUp(movementSpeed);
+            }
+            if (Keyboard.isKeyDown(Keyboard.KEY_LSHIFT)) {
+                camera.moveDown(movementSpeed);
+            }
+
+            glLoadIdentity();
+            camera.lookThrough();
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+            render();
+            Display.update();
+            Display.sync(60);
+        }
+
+        Display.destroy();
+    }
+
+    private void render() {
+        try {
+            glBegin(GL_QUADS);
+            //Top
+            glColor3f(0.0f, 0.0f, 1.0f);
+            glVertex3f(1.0f, 1.0f, -1.0f);
+            glVertex3f(-1.0f, 1.0f, -1.0f);
+            glVertex3f(-1.0f, 1.0f, 1.0f);
+            glVertex3f(1.0f, 1.0f, 1.0f);
+            //Bottom
+            glColor3f(1.0f, 0.0f, 1.0f);
+            glVertex3f(1.0f, -1.0f, 1.0f);
+            glVertex3f(-1.0f, -1.0f, 1.0f);
+            glVertex3f(-1.0f, -1.0f, -1.0f);
+            glVertex3f(1.0f, -1.0f, -1.0f);
+            //Front
+            glColor3f(0.0f, 1.0f, 1.0f);
+            glVertex3f(1.0f, 1.0f, 1.0f);
+            glVertex3f(-1.0f, 1.0f, 1.0f);
+            glVertex3f(-1.0f, -1.0f, 1.0f);
+            glVertex3f(1.0f, -1.0f, 1.0f);
+            //Back
+            glColor3f(0.0f, 1.0f, 1.0f);
+            glVertex3f(1.0f, -1.0f, -1.0f);
+            glVertex3f(-1.0f, -1.0f, -1.0f);
+            glVertex3f(-1.0f, 1.0f, -1.0f);
+            glVertex3f(1.0f, 1.0f, -1.0f);
+            //Left
+            glColor3f(1.0f, 1.0f, 1.0f);
+            glVertex3f(-1.0f, 1.0f, 1.0f);
+            glVertex3f(-1.0f, 1.0f, -1.0f);
+            glVertex3f(-1.0f, -1.0f, -1.0f);
+            glVertex3f(-1.0f, -1.0f, 1.0f);
+            //Right
+            glColor3f(1.0f, 1.0f, 0.0f);
+            glVertex3f(1.0f, 1.0f, -1.0f);
+            glVertex3f(1.0f, 1.0f, 1.0f);
+            glVertex3f(1.0f, -1.0f, 1.0f);
+            glVertex3f(1.0f, -1.0f, -1.0f);
+            glEnd();
+            
+            //drawing wireframe
+            glBegin(GL_LINE_LOOP);
+            //Top
+            glColor3f(0.0f, 0.0f, 0.0f);
+            glVertex3f(1.0f, 1.0f, -1.0f);
+            glVertex3f(-1.0f, 1.0f, -1.0f);
+            glVertex3f(-1.0f, 1.0f, 1.0f);
+            glVertex3f(1.0f, 1.0f, 1.0f);
+            glEnd();
+            glBegin(GL_LINE_LOOP);
+            //Bottom
+            glVertex3f(1.0f, -1.0f, 1.0f);
+            glVertex3f(-1.0f, -1.0f, 1.0f);
+            glVertex3f(-1.0f, -1.0f, -1.0f);
+            glVertex3f(1.0f, -1.0f, -1.0f);
+            glEnd();
+            glBegin(GL_LINE_LOOP);
+            //Front
+            glVertex3f(1.0f, 1.0f, 1.0f);
+            glVertex3f(-1.0f, 1.0f, 1.0f);
+            glVertex3f(-1.0f, -1.0f, 1.0f);
+            glVertex3f(1.0f, -1.0f, 1.0f);
+            glEnd();
+            glBegin(GL_LINE_LOOP);
+            //Back
+            glVertex3f(1.0f, -1.0f, -1.0f);
+            glVertex3f(-1.0f, -1.0f, -1.0f);
+            glVertex3f(-1.0f, 1.0f, -1.0f);
+            glVertex3f(1.0f, 1.0f, -1.0f);
+            glEnd();
+            glBegin(GL_LINE_LOOP);
+            //Left
+            glVertex3f(-1.0f, 1.0f, 1.0f);
+            glVertex3f(-1.0f, 1.0f, -1.0f);
+            glVertex3f(-1.0f, -1.0f, -1.0f);
+            glVertex3f(-1.0f, -1.0f, 1.0f);
+            glEnd();
+            glBegin(GL_LINE_LOOP);
+            //Right
+            glVertex3f(1.0f, 1.0f, -1.0f);
+            glVertex3f(1.0f, 1.0f, 1.0f);
+            glVertex3f(1.0f, -1.0f, 1.0f);
+            glVertex3f(1.0f, -1.0f, -1.0f);
+            glEnd();
+        } catch (Exception e) {
+        }
+    }
+}
